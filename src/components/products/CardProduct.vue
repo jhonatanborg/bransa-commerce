f05bbc920d904bdd41c90913c84ca98d480319fa
jhonatanborg <jhonatanborgesdj@gmail.com>
2020-12-11 14:47:23 -0400

    three lines name products

--- src/components/products/CardProduct.vue
+++ src/components/products/CardProduct.vue
@@ -73,7 +73,7 @@ export default {
   overflow: hidden;
   text-overflow: ellipsis;
   display: -webkit-box;
-  -webkit-line-clamp: 2; /* number of lines to show */
+  -webkit-line-clamp: 3; /* number of lines to show */
   -webkit-box-orient: vertical;
 }
 </style>
